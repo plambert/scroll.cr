16ffef4587313b96593690b4abdbb0c2617732d2
Paul M. Lambert <plambert@plambert.net>
2026-07-18 20:28:42 -0700

    initial commit

---
+++ spec/scroll_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Scroll do
+  # TODO: Write tests
+
+  it "works" do
+    false.should be_true
+  end
+end
---
+++ src/scroll.cr
@@ -0,0 +1,8 @@
+# TODO: Write documentation for `Scroll`
+module Scroll
+  {% begin %}
+  VERSION = {{ `shards version`.strip.stringify }}
+  {% end %}
+
+  # TODO: Put your code here
+end
